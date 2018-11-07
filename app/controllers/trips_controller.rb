4949ae7bb82f9bde8a1f6e05735f225bf198f75e
Emily Pancake <epancake24@gmail.com>
2018-11-07 14:08:55 -0700

    first form working

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,24 @@
+class TripsController < ApplicationController
+    def index
+        @trips = Trip.all
+    end
+    
+    def show
+        @trip = Trip.find(params[:id])
+    end
+    
+    def new
+    end
+
+    def create
+        @trip = Trip.new(trip_params)
+       
+        @trip.save
+        redirect_to @trip
+    end
+
+    private
+        def trip_params
+            params.require(:trip).permit(:start, :end, :miles)
+        end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+
+  resources :trips
+
+  root 'welcome#index'
+end
